6e30f327613d20b85e85f53a72c85e768d2ba7b8
soleu <dlthf555@gmail.com>
2022-03-02 00:10:47 +0900

    add : 값타입 비교

---
+++ jpa-basic/src/main/java/hellojpa/ValueMain.java
@@ -0,0 +1,14 @@
+package hellojpa;
+
+public class ValueMain {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 10;
+        System.out.println("a == b : " + (a == b));//true
+
+        Address address1 = new Address("city", "street", "10000");
+        Address address2 = new Address("city", "street", "10000");
+        System.out.println("address1 == address2 : " + (address1 == address2)); //false
+        System.out.println("address1 equals address2 : " + address1.equals(address2));//기본적으로는 == 비교. override 해야함
+    }
+}
