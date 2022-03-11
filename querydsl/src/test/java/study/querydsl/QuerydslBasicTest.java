64f3ebced396f1699fda214ba6841f5ed2960968
soleu <dlthf555@gmail.com>
2022-03-11 20:50:35 +0900

    add : 기본 조인

--- querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
+++ querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -204,7 +204,39 @@ public class QuerydslBasicTest {
 
         assertThat(teamB.get(team.name)).isEqualTo("teamB");
         assertThat(teamB.get(member.age.avg())).isEqualTo(35);
-
     }
 
+    /*
+    팀 A에 소속된 모든 회원
+     */
+    @Test
+    public void join() {
+        JPAQueryFactory result = this.queryFactory;
+        result
+                .selectFrom(member)
+                .join(member.team, team)
+                .where(team.name.eq("teamA"))
+                .fetch();
+
+//        assertThat(result)
+//                .extracting("username")
+//                .containsExactly("member1","member2");
+    }
+
+    //세타 조인(연관관계가 없어도 조인 가능)
+    //회원의 이름이 팀 이름과 같은 회원 조회
+    @Test
+    public void theta_join() {
+        em.persist(new Member("teamA"));
+        em.persist(new Member("teamB"));
+        List<Member> result = queryFactory
+                .select(member)
+                .from(member, team)
+                .where(member.username.eq(team.name))
+                .fetch();
+
+        assertThat(result)
+                .extracting("username")
+                .containsExactly("teamA", "teamB");
+    }
 }
