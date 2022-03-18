1f82ca3f4e00f120c0a5dd48729803ff75034a4d
soleu <dlthf555@gmail.com>
2022-03-19 03:27:18 +0900

    add : 상품 엔티티 추가

--- jpa_practice/jpashop/src/main/java/jpabook/jpashop/domain/Item.java
+++ jpa_practice/jpashop/src/main/java/jpabook/jpashop/domain/Item.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.domain;
 
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -21,4 +22,29 @@ public abstract class Item {
     private int stockQuantity;
     @ManyToMany(mappedBy = "items")
     private List<Category> categories;
+
+    //--비즈니스 로직==// : 도메인 설계에서 엔티티자체에서 해결할 수 있는 것들은 엔티티 안에 로직을 넣는것이 좋음(객체지향적)
+//    -> setter를 가지고 어떻게 하는게 아니라 엔티티 안에서 지지고 볶기
+
+    /**
+     * 재고 수량 증가
+     *
+     * @param quantity
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * 재고 수량 감소
+     *
+     * @param quantity
+     */
+    public void removeStock(int quantity) {
+        int resStock = this.stockQuantity - quantity;
+        if (resStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = resStock;
+    }
 }
