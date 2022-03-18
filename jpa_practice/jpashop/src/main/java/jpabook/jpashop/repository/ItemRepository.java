4ee2c93f34bc74a6f5c56b71df3ca51b4e8fb76f
soleu <dlthf555@gmail.com>
2022-03-19 03:34:50 +0900

    add : item repo,service 추가

---
+++ jpa_practice/jpashop/src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -0,0 +1,31 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+    private final EntityManager em;
+
+    public void save(Item item) {
+        if (item.getId() == null) { //jpa에 없음(새로 생성하는 객체)
+            em.persist(item); //신규 등록
+        } else { //db에 등록되어있음
+            em.merge(item);//update
+        }
+    }
+
+    public Item findOne(Long id) {
+        return em.find(Item.class, id);
+    }
+
+    public List<Item> findAll() {
+        return em.createQuery("select i from Item i", Item.class)
+                .getResultList();
+    }
+}
