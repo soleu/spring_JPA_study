944132c44f821ce489405c287d390e36245cea31
soleu <dlthf555@gmail.com>
2022-03-20 00:11:28 +0900

    add : order entity

--- jpa_practice/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ jpa_practice/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -1,5 +1,8 @@
 package jpabook.jpashop.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -8,6 +11,8 @@ import java.util.List;
 import static javax.persistence.FetchType.*;
 
 @Entity
+@Getter
+@Setter
 @Table(name = "orders")
 public class Order {
     @Id
@@ -33,14 +38,53 @@ public class Order {
         member.getOrders().add(this);
     }
 
-    public void addOrderItem(OrderItem orderItem){
+    public void addOrderItem(OrderItem orderItem) {
         orderItems.add(orderItem);
         orderItem.setOrder(this);
     }
 
-    public void setDelivery(Delivery delivery){
-        this.delivery=delivery;
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
         delivery.setOrder(this);
+    }
+
+    //==생성 메서드==// -> 생성하는 시점에 createOrder만 바꾸면 돼서 편함
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for (OrderItem orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+    //==비즈니스 로직==//
+
+    /**
+     * 주문 취소
+     */
+    public void cancel() {
+        if (delivery.getStatus() == DeliveryStatus.COMP) {
+            throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다.");
+        }
+        this.setStatus(OrderStatus.CANCEL);
+        for (OrderItem orderItem : orderItems) {
+            orderItem.cancel();
+        }
+    }
+    //==조회 로직==//
+
+    /**
+     * 전체 주문 가격 조회
+     */
+    public int getTotalPrice() {
+        //자바 8 문법 ** for문 -> stream
+        int totalPrice = orderItems.stream()
+                .mapToInt(OrderItem::getTotalPrice)
+                .sum();
+        return totalPrice;
     }
 }
 
--- jpa_practice/jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ jpa_practice/jpashop/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -22,7 +22,30 @@ public class OrderItem {
     @JoinColumn(name = "order_id")
     private Order order;
 
-    private int orderPrice;
-    private int count;
+    private int orderPrice; //주문 가격
+    private int count; // 주문 수량
 
+    //==생성 메서드==//
+    public static OrderItem createOrderItem(Item item,int orderPrice,int count){
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);  
+        return orderItem;
+    }
+
+    //==비즈니스 로직==//
+    public void cancel() { //상품 주문 취소 -> 다시 재고 늘림
+        getItem().addStock(count);
+    }
+    //==조회 로직==//
+
+    /**
+     * 주문 상품 전체 가격 조회
+     */
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
