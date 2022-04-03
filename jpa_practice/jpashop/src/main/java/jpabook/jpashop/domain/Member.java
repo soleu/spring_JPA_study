afbcef29ca2a2f958af75fcf6e388cf6a04e66fa
soleu <dlthf555@gmail.com>
2022-04-04 02:11:15 +0900

    add : 회원 등록 api

--- jpa_practice/jpashop/src/main/java/jpabook/jpashop/domain/Member.java
+++ jpa_practice/jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,7 +15,9 @@ public class Member {
     @Column(name="member_id")
     private Long id;
 
+    @NotEmpty  //검증로직이 entity에 다 들어가게 됨, 또는 변수명을 바꾸게 되면 api spec 전체가 바뀜. => api dto 사용 권장(엔티티 바인딩 지양)
     private String name;
+
     @Embedded
     private Address address;
 
